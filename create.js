2f7316f89e14b9c025cb13ee3fcf42ade0ff10e5
ndewmanuel66-ship-it <ndew.manuel66@gmail.com>
2025-11-14 13:25:55 +0700

    Add scroll-to-top button functionality

--- create.js
+++ create.js
@@ -17,3 +17,14 @@ if (form) {
     form.reset();
   });
 }
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
